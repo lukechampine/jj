7e7751d87364276e010a121fbae2a68011a906b0
lukechampine <luke.champine@gmail.com>
2017-02-16 04:05:26 -0500

    add TestJournal

--- jj_test.go
+++ jj_test.go
@@ -19,6 +19,59 @@ func tempFile(t interface {
 	}
 }
 
+func tempJournal(t interface {
+	Fatal(...interface{})
+}, obj interface{}, name string) (*Journal, func()) {
+	f, err := ioutil.TempFile("", name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	f.Close()
+	j, err := OpenJournal(f.Name(), obj)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return j, func() {
+		j.Close()
+		os.RemoveAll(f.Name())
+	}
+}
+
+func TestJournal(t *testing.T) {
+	type bar struct {
+		Z int `json:"z"`
+	}
+	type foo struct {
+		X int   `json:"x"`
+		Y []bar `json:"y"`
+	}
+
+	j, cleanup := tempJournal(t, foo{Y: []bar{}}, "TestJournal")
+	defer cleanup()
+
+	us := []Update{
+		NewUpdate("x", 7),
+		NewUpdate("y.0", bar{}),
+		NewUpdate("y.0.z", 3),
+	}
+	if err := j.Update(us); err != nil {
+		t.Fatal(err)
+	}
+	if err := j.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	var f foo
+	j2, err := OpenJournal(j.filename, &f)
+	if err != nil {
+		t.Fatal(err)
+	}
+	j2.Close()
+	if f.X != 7 || len(f.Y) != 1 || f.Y[0].Z != 3 {
+		t.Fatal("OpenJournal applied updates incorrectly:", f)
+	}
+}
+
 func TestRewritePath(t *testing.T) {
 	tests := []struct {
 		json string
